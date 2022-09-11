5cb589ff37645806c96add33e5908cb6e7fed2fe
David-Cardozo <113269686+David-Cardozo@users.noreply.github.com>
2022-09-10 22:47:51 -0500

    Add files via upload

---
+++ Trabajo #2/Informacion.js
@@ -0,0 +1,2 @@
+const informacion1 = ["David Cardozo", 16, true, new Date("August 22 2006"), "El principito", "Antoine de Saint-Exupéry", "https://www.descubrelima.pe/wp-content/uploads/2020/03/EL-PRINCIPITO.pdf"  ]
+
